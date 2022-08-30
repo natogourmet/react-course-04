b6a89513b3a06c7136db73a7f1db5e9cc033c10e
natogourmet <natogourmet@hotmail.com>
2022-08-30 00:29:48 -0500

    filter repeated

--- src/GifExpertApp.tsx
+++ src/GifExpertApp.tsx
@@ -5,6 +5,7 @@ export const GifExpertApp = () => {
   const [categories, setCategories] = useState(['One Punch', 'Samurai X']);
 
   const addCategory = ( category: string ) => {
+    if (categories.includes(category)) return;
     setCategories([...categories, category]);
   };
 
@@ -14,8 +15,8 @@ export const GifExpertApp = () => {
       <AddCategory onInputSubmit={addCategory} />
       <h1>Gif Expert App</h1>
       <ol>
-        {categories.map((category, index) => {
-          return <li key={index}>{category}</li>;
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
         })}
       </ol>
     </>
