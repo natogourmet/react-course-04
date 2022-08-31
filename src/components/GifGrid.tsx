8777fa3989e27df39b7e62b39be8462e1bfbca8a
natogourmet <natogourmet@hotmail.com>
2022-08-31 12:11:31 -0500

    isLoading implemented

--- src/components/GifGrid.tsx
+++ src/components/GifGrid.tsx
@@ -8,14 +8,12 @@ type props = {
 };
 
 export const GifGrid = ({ category }: props) => {
-
-  const {images, isLoading } = useFetchGifs( { category } );
-  
- 
+  const { images, isLoading } = useFetchGifs({ category });
 
   return (
     <>
       <h3>{category}</h3>
+      {isLoading && <h2>Loading...</h2>}
       <div className="card-grid">
         {images.map((img: any) => (
           <GifItem key={img.id} {...img} />
